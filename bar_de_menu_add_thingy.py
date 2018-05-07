624ec17755bccbe232b867d502439f9b21b7a731
adouniH <Houssemadouni11@gmail.com>
2018-05-07 15:54:43 +0200

    more about menu Bar

--- bar_de_menu_add_thingy.py
+++ bar_de_menu_add_thingy.py
@@ -8,19 +8,32 @@ class Window(QtGui.QMainWindow):
         self.setWindowTitle("Houssem ADOUNI")
         self.setWindowIcon(QtGui.QIcon('ENSG.png'))
 
-        """"
+        """
         ADD Thingy to menu bar
-        """"
+        """
         extractAction = QtGui.QAction("&do something", self)
         extractAction.setShortcut("CTRL+Q")
         extractAction.setStatusTip('Leave the app')
         extractAction.triggered.connect(self.close_application)
 
+        extractAction1 = QtGui.QAction("&do something", self)
+        extractAction1.setShortcut("CTRL+Q")
+        extractAction1.setStatusTip('Leave the app')
+        extractAction1.triggered.connect(self.close_application)
+
+        extractAction2 = QtGui.QAction("&do one other thing", self)
+        extractAction2.setShortcut("CTRL+Q")
+        extractAction2.setStatusTip('Leave the app')
+        extractAction2.triggered.connect(self.close_application)
 
         self.statusBar()
         mainMenu = self.menuBar()
-        fileMenu = mainMenu.addMenu('&File')
+        fileMenu = mainMenu.addMenu('&Menu1')
         fileMenu.addAction(extractAction)
+
+        fileMenu1 = mainMenu.addMenu('&Menu2')
+        fileMenu1.addAction(extractAction1)
+        fileMenu1.addAction(extractAction2)
 
         """
         finish of the thingy
