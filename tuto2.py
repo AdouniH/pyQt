c30f27fd903923e68f64dde218993d27a8570f01
adouniH <Houssemadouni11@gmail.com>
2018-05-07 14:58:28 +0200

    button and click connection

--- tuto2.py
+++ tuto2.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt4 import QtGui
+from PyQt4 import QtGui, QtCore
 
 class Window(QtGui.QMainWindow):
     def __init__(self):
@@ -7,8 +7,19 @@ class Window(QtGui.QMainWindow):
         self.setGeometry(800,50,500,300)
         self.setWindowTitle("Houssem ADOUNI")
         self.setWindowIcon(QtGui.QIcon('ENSG.png'))
+        self.home()
+
+    def home(self):
+        btn=QtGui.QPushButton("Quit",self)
+        btn.clicked.connect(QtCore.QCoreApplication.instance().quit)
+        btn.resize(100,100)
+        btn.move(100,100)
         self.show()
 
-app = QtGui.QApplication(sys.argv)
-GUI = Window()
-sys.exit(app.exec_())
+def run():
+    app = QtGui.QApplication(sys.argv)
+    GUI = Window()
+    sys.exit(app.exec_())
+
+
+run()
